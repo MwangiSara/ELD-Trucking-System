30e8f5950f193da3f8c6e44644ea73e362366dc4
MwangiSara <mwangisarah113@gmail.com>
2025-09-18 00:56:43 +0300

    fix: hosts

--- eld_backend/eld_backend/settings.py
+++ eld_backend/eld_backend/settings.py
@@ -112,14 +112,10 @@ REST_FRAMEWORK = {
     'DEFAULT_VERSIONING_CLASS': 'rest_framework.versioning.URLPathVersioning',
 }
 
-# for development
-# CORS_ALLOWED_ORIGINS = [
-#     "http://localhost:5173",
-#     "http://127.0.0.1:5173",
-# ]
-
 
 CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173", # for development
+    "http://127.0.0.1:5173",
     "https://eld-trucking-system.vercel.app"
 ]
 
